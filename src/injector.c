649b08a3b345f67ee522cc303bf2910946d19db1
methneviebyvaet <77743895+meth1337@users.noreply.github.com>
2023-03-28 09:20:17 +0300

    added target process state restoration

--- src/injector.c
+++ src/injector.c
@@ -93,13 +93,21 @@ int main(int argc, char** argv) {
 
     // wait for the process to stop again
     waitpid(pid, NULL, 0);
-
-    // detach from the target process
+	
+	// detach from the target process
     if (ptrace(PTRACE_DETACH, pid, NULL, NULL) == -1) {
         die("[!] failed to detach from target process");
     }
 
     printf("[*] library injected successfully!\n");
 
+    // restore the target process's state
+    if (kill(pid, SIGCONT) == -1) {
+        printf("[!] failed to resume the target process: %d\n", pid);
+    } else {
+        printf("[*] resumed the target process: %d\n", pid);
+    }
+
     return 0;
 }
+
