8d16be582c4903065dc6ced272bd94a076f15078
methneviebyvaet <77743895+meth1337@users.noreply.github.com>
2023-04-12 20:43:16 +0300

    CAPITALIZATION

--- src/injector.c
+++ src/injector.c
@@ -13,21 +13,21 @@
 #define BUF_SIZE 256
 #define RTLD_NEXT ((void *) -1l)
 
-// better error handler
+// Better error handler
 void die(char* message) {
     perror(message);
     exit(EXIT_FAILURE);
 }
 
 int main(int argc, char** argv) {
-    // sudo check!
+    // Sudo check!
     if (geteuid() != 0) {
-        printf("[!] please run this program as sudo!\n");
+        printf("[!] Please run this program as sudo!\n");
         exit(EXIT_FAILURE);
     }
 
     if (argc < 3) {
-        printf("usage: %s <lib_path> <pid>\n", argv[0]);
+        printf("Usage: %s <lib_path> <pid>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
@@ -35,77 +35,77 @@ int main(int argc, char** argv) {
     strncpy(lib_path, argv[1], BUF_SIZE);
     int pid = atoi(argv[2]);
 
-    // lib path check
+    // Lib path check
     if (access(lib_path, F_OK) == -1) {
-        die("[!] invalid library path");
+        die("[!] Invalid library path");
     }
 
-    // pid check
+    // Pid check
     if (kill(pid, 0) == -1) {
-        die("[!] invalid pid");
+        die("[!] Invalid pid");
     }
 
-    // attach to the target process
+    // Attach to the target process
     if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) {
-        die("[!] failed to attach to process");
+        die("[!] Failed to attach to process");
     }
 
-    // wait for the process to stop
+    // Wait for the process to stop
     waitpid(pid, NULL, 0);
 
-    // get the address of the dlopen function in the target process
+    // Get the address of the dlopen function in the target process
     void* dlopen_addr = dlsym(RTLD_NEXT, "dlopen");
     if (dlopen_addr == NULL) {
-        die("[!] failed to locate dlopen function in target process");
+        die("[!] Failed to locate dlopen function in target process");
     }
-    printf("[*] dlopen memory address: %p\n", dlopen_addr);
+    printf("[*] Dlopen memory address: %p\n", dlopen_addr);
 
-    // allocate space in the target process for the path to the library
+    // Allocate space in the target process for the path to the library
     void* lib_path_addr = (void*) calloc(BUF_SIZE, sizeof(char));
     if (lib_path_addr == NULL) {
-        die("[!] failed to allocate memory in target process");
+        die("[!] Failed to allocate memory in target process");
     }
 
-    // write the library path to the allocated space in the target process
+    // Write the library path to the allocated space in the target process
     strncpy((char*) lib_path_addr, lib_path, BUF_SIZE);
-    printf("[*] found library path: %s\n", (char*) lib_path_addr);
+    printf("[*] Found library path: %s\n", (char*) lib_path_addr);
 
-    // get the register values from the target process
+    // Get the register values from the target process
     struct user_regs_struct regs64;
     if (ptrace(PTRACE_GETREGS, pid, 0, &regs64) == -1) {
-        die("[!] failed to get register values from target process");
+        die("[!] Failed to get register values from target process");
     }
 
-    // set the arguments for the dlopen function call in the target process
+    // Set the arguments for the dlopen function call in the target process
     regs64.rdi = (unsigned long long) lib_path_addr;
     regs64.rsi = RTLD_NOW;
     regs64.rip = (unsigned long long) dlopen_addr;
 
-    // set the register values in the target process
+    // Set the register values in the target process
     if (ptrace(PTRACE_SETREGS, pid, 0, &regs64) == -1) {
-        die("[!] failed to set register values in target process");
+        die("[!] Failed to set register values in target process");
     }
 
-    // continue execution of the target process
+    // Continue execution of the target process
     if (ptrace(PTRACE_CONT, pid, 0, 0) == -1) {
-        die("[!] failed to continue target process");
+        die("[!] Failed to continue target process");
     }
 
-    // wait for the process to stop again
+    // Wait for the process to stop again
     waitpid(pid, NULL, 0);
 	
-	// detach from the target process
+    // Detach from the target process
     if (ptrace(PTRACE_DETACH, pid, NULL, NULL) == -1) {
-        die("[!] failed to detach from target process");
+        die("[!] Failed to detach from target process");
     }
 
-    printf("[*] library injected successfully!\n");
+    printf("[*] Library injected successfully!\n");
 
-    // restore the target process's state
+    // Restore the target process's state
     if (kill(pid, SIGCONT) == -1) {
-        printf("[!] failed to resume the target process: %d\n", pid);
+        printf("[!] Failed to resume the target process: %d\n", pid);
     } else {
-        printf("[*] resumed the target process: %d\n", pid);
+        printf("[*] Resumed the target process: %d\n", pid);
     }
 
     return 0;
